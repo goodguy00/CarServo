82b3fbf609b77d00c15c7063eecd8d66346f656e
Petr <psmely@chlnx8.smely.org>
2017-06-14 22:03:53 +0200

    bla

---
+++ CarServo.cpp
@@ -0,0 +1,121 @@
+/*
+ * Library for moving the car through L298n board
+ */
+
+#include "CarServo.h"
+
+CarServo::CarServo(){
+}
+
+void CarServo::begin(
+    int in1
+  , int in2
+  , int in3
+  , int in4
+){
+  pinRF=in1;
+  pinRB=in2;
+  pinLB=in3;
+  pinLF=in4;
+  pinMode(pinRB,OUTPUT);
+  pinMode(pinRF,OUTPUT);
+  pinMode(pinLB,OUTPUT);
+  pinMode(pinLF,OUTPUT);
+//  Serial.begin(9600);
+}
+
+void CarServo::foreward(int speed = _PWM_MAX)
+{
+  analogWrite(pinRB,_PWM_MIN);
+  analogWrite(pinRF,speed);
+  analogWrite(pinLB,_PWM_MIN);
+  analogWrite(pinLF,speed);
+  delay(move_delay * 100);
+//   Serial.print("FWD::");
+//   Serial.print(pinRF);
+//   Serial.print("::");
+//   Serial.println(speed);
+}
+
+void CarServo::backward(int speed = _PWM_MAX)
+{
+  analogWrite(pinRB,speed);
+  analogWrite(pinRF,_PWM_MIN);
+  analogWrite(pinLB,speed);
+  analogWrite(pinLF,_PWM_MIN);
+  delay(move_delay * 100);
+//   Serial.print("BCK::");
+//   Serial.print(pinRF);
+//   Serial.print("::");
+//   Serial.println(speed);
+}
+
+void CarServo::fullstop()
+{
+  analogWrite(pinRB,_PWM_MAX);
+  analogWrite(pinRF,_PWM_MAX);
+  analogWrite(pinLB,_PWM_MAX);
+  analogWrite(pinLF,_PWM_MAX);
+  delay(move_delay * 100);
+}
+
+void CarServo::goleft(int speed = _PWM_MAX)
+{
+  analogWrite(pinRB,_PWM_MIN);
+  analogWrite(pinRF,speed);
+  analogWrite(pinLB,speed/2);
+  analogWrite(pinLF,speed);
+  delay(move_delay * 100);
+}
+
+void CarServo::goright(int speed = _PWM_MAX)
+{
+  analogWrite(pinRB,speed/2);
+  analogWrite(pinRF,speed);
+  analogWrite(pinLB,_PWM_MIN);
+  analogWrite(pinLF,speed);
+  delay(move_delay * 100);
+}
+
+void CarServo::backleft(int speed = _PWM_MAX)
+{
+  analogWrite(pinRB,speed);
+  analogWrite(pinRF,_PWM_MIN);
+  analogWrite(pinLB,speed);
+  analogWrite(pinLF,speed/2);
+  delay(move_delay * 100);
+}
+
+void CarServo::backright(int speed = _PWM_MAX)
+{
+  analogWrite(pinRB,speed);
+  analogWrite(pinRF,speed/2);
+  analogWrite(pinLB,speed);
+  analogWrite(pinLF,_PWM_MIN);
+  delay(move_delay * 100);
+}
+
+void CarServo::turnleft(int speed = _PWM_FAST)
+{
+  analogWrite(pinRB,_PWM_MIN);
+  analogWrite(pinRF,speed);
+  analogWrite(pinLB,speed);
+  analogWrite(pinLF,_PWM_MIN);
+  delay(move_delay * 100);
+}
+
+void CarServo::turnright(int speed = _PWM_FAST)
+{
+  analogWrite(pinRB,speed);
+  analogWrite(pinRF,_PWM_MIN);
+  analogWrite(pinLB,_PWM_MIN);
+  analogWrite(pinLF,speed);
+  delay(move_delay * 100);
+}
+
+void CarServo::set_delay(int setd)
+{
+  move_delay=setd;
+}
+
+CarServo Car=CarServo();
---
+++ CarServo.h
@@ -0,0 +1,64 @@
+/*
+ * Library for moving the car through L298n board
+ *
+ * PINS on the board
+ * EN A : enable motor A (right)
+ * IN 1 : backward :: pinRB
+ * IN 2 : foreward :: pinRF
+ * EN B : enable motor B (left)
+ * IN 3 : foreward :: pinLF
+ * IN 4 : backward :: pinLB
+ *
+ * Tipps: set EN-pins to H and regulate speed with PWM (analogWrite) on the digitalPins
+ */
+
+#ifndef CarServo_h
+#define CarServo_h
+
+#include <Servo.h>
+#include <Arduino.h>
+
+// defaults if not given otherwise
+#define _pinRB 11
+#define _pinRF 10
+#define _pinLF 9
+#define _pinLB 6
+
+#define _PWM_MIN  0
+#define _PWM_SLOW 100
+#define _PWM_FAST 191
+#define _PWM_MAX  255
+
+#define _move_delay 1
+
+class CarServo{
+        public:
+                CarServo();
+                void begin(
+                      int in1
+                    , int in2
+                    , int in3
+                    , int in4
+                );
+                void foreward(int speed = _PWM_MAX);
+                void backward(int speed = _PWM_MAX);
+                void fullstop();
+                void goright(int speed = _PWM_MAX);
+                void goleft(int speed = _PWM_MAX);
+                void backright(int speed = _PWM_MAX);
+                void backleft(int speed = _PWM_MAX);
+                void turnright(int speed = _PWM_FAST);
+                void turnleft(int speed = _PWM_FAST);
+                void set_delay(int setd); // *100 in ms
+        private:
+                int move_delay = _move_delay;
+                int pinRB; // in 1
+                int pinRF; // in 2
+                int pinLB; // in 3
+                int pinLF; // in 4
+
+};
+
+extern CarServo Car;
+
+#endif
